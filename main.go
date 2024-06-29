1c95e0f5a9e420f0364131701572d2209adb25b8
Ann <Ann1879@yandex.ru>
2024-06-29 14:09:09 +0300

    Calculator Arabic & Roman

--- main.go
+++ main.go
@@ -2,29 +2,95 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-func ComputerNumber(number int) string {
-	if number > 9 && number < 20 {
-		return fmt.Sprintf("%d компьютеров", number)
-	} else {
-		switch number % 10 {
-		case 1:
-			return fmt.Sprintf("%d компьютер", number)
-		case 2, 3, 4:
-			return fmt.Sprintf("%d компьютера", number)
-		default:
-			return fmt.Sprintf("%d компьютеров", number)
+var romToAr = map[string]int{
+	"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5,
+	"VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
+}
+
+func operator(oper string, a, b int) int {
+	var result int
+	switch oper {
+	case "+":
+		result = a + b
+	case "-":
+		result = a - b
+	case "/":
+		result = a / b
+	case "*":
+		result = a * b
+	}
+	return result
+}
+
+func resultRome(resultInt int) string {
+	var resultString string
+	for resultInt > 0 {
+		if resultInt >= 100 {
+			resultString += "C"
+			resultInt -= 100
+		} else if resultInt >= 90 {
+			resultString += "XC"
+			resultInt -= 90
+		} else if resultInt >= 50 {
+			resultString += "L"
+			resultInt -= 50
+		} else if resultInt >= 40 {
+			resultString += "XL"
+			resultInt -= 40
+		} else if resultInt >= 10 {
+			resultString += "X"
+			resultInt -= 10
+		} else if resultInt >= 9 {
+			resultString += "IX"
+			resultInt -= 9
+		} else if resultInt >= 5 {
+			resultString += "V"
+			resultInt -= 5
+		} else if resultInt >= 4 {
+			resultString += "IV"
+			resultInt -= 4
+		} else if resultInt >= 1 {
+			resultString += "I"
+			resultInt -= 1
 		}
 	}
+
+	return resultString
 }
 
 func main() {
-	var number int
+	var str string
+	fmt.Scan(&str)
+	str = strings.ReplaceAll(str, " ", "")
+	re := regexp.MustCompile(`^(\d+|[IVX]+)([\+\-\*/])(\d+|[IVX]+)$`)
+	part := re.FindStringSubmatch(str)
+	if len(part) < 3 {
+		panic("Выдача паники, так как строка не является математической операцией..")
+	}
+	if len(part) != 4 {
+		panic("Выдача паники, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
+	}
 
-	fmt.Print("Введите число: ")
-	fmt.Scan(&number)
-
-	result := ComputerNumber(number)
-	fmt.Println(result)
+	if romToAr[part[1]] != 0 && romToAr[part[3]] != 0 {
+		aInt := romToAr[part[1]]
+		bInt := romToAr[part[3]]
+		resultInt := operator(part[2], aInt, bInt)
+		if resultInt < 1 {
+			panic("Выдача паники, так как в римской системе нет отрицательных чисел.")
+		}
+		fmt.Println(resultRome(resultInt))
+	}
+	if romToAr[part[1]] != 0 && romToAr[part[3]] == 0 || romToAr[part[1]] == 0 && romToAr[part[3]] != 0 {
+		panic("Выдача паники, так как используются одновременно разные системы счисления.")
+	}
+	if romToAr[part[1]] == 0 || romToAr[part[3]] == 0 {
+		intA, _ := strconv.Atoi(part[1])
+		intB, _ := strconv.Atoi(part[3])
+		fmt.Println(operator(part[2], intA, intB))
+	}
 }
